44b88e65cf45ac89825b4aecc169b32225009fae
leofreestyle <46056371+leofreestyle@users.noreply.github.com>
2019-04-03 17:58:39 -0300

    Mejora!

--- Program.cs
+++ Program.cs
@@ -5,28 +5,21 @@ namespace Trabajo1
     class Program
     {
         static void Main(string[] args)
-        {
+        {                                      
             double[] numeros = new double[10];
             double? mayor = null;
             double? menor = null;
+            double numero = 0;
             double promedio = 0;
             double acumulador = 0;
             Console.WriteLine("Ingrese 10 numeros: ");
             for (int i = 0; i < numeros.Length; i++)
-            {
-                numeros[i] = Convert.ToDouble(Console.ReadLine());
-            }
-
-            Console.WriteLine("Al revez: ");
-
-            for (int i = numeros.Length - 1; i >= 0; i--)
-            {
-                Console.WriteLine(numeros[i]);
-            }
-
-            
-            for (int i = 0; i < numeros.Length; i++)
-            {
+            {   
+                while (!double.TryParse(Console.ReadLine(), out numero))
+                { 
+                    Console.WriteLine("Ingrese solo numeros: " );
+                }
+                numeros[i] = numero;
                 if (mayor == null)
                 {
                     mayor = numeros[i];
@@ -38,8 +31,6 @@ namespace Trabajo1
                         mayor = numeros[i];
                     }
                 }
-                
-
                 if (menor == null)
                 {
                     menor = numeros[i];
@@ -50,21 +41,18 @@ namespace Trabajo1
                     {
                         menor = numeros[i];
                     }
-                    
                 }
-
                 acumulador = acumulador + numeros[i];
-                
-                
-                
+            }
+            Console.WriteLine("Al revez: ");
+            for (int i = numeros.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(numeros[i]);
             }
             promedio = acumulador / numeros.Length;
             Console.WriteLine("El mayor: " + mayor);
             Console.WriteLine("El menor: " + menor);
             Console.WriteLine("El promedio: " + promedio);
-
-
-
             Console.ReadKey();
         }
     }
